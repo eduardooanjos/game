259e4d77b4f9e159e4151fdeb668f5eca6fe9484
danielmagalhaesprando <danielmagalhaesprando@gmail.com>
2026-03-29 14:01:59 -0300

    Versão 3

--- Versao3/jogador.py
+++ Versao3/jogador.py
@@ -1,9 +1,12 @@
+# Classe de cada um dos 10 jogadores
 class jogador:
     def __init__(self, nome, moedas, result):
-        self.nome = nome
-        self.moedas = moedas
-        self.result = result
+        self.nome = nome        # atributo nome (ex.: Jogador1, Jogador2, ...)
+        self.moedas = moedas    # atributo da quantidade de moedas atual, a ser alterado ao decorrer das rodadas
+        self.result = result    # atributo do resultado de uma rodada, somente sendo alterado se jogador participar da rodada atual
     
+    # Getters:
+
     def getsaldo(self):
         return self.moedas
     
@@ -12,6 +15,8 @@ class jogador:
     
     def getresult(self):
         return self.result
+
+    # Setters:
 
     def setresult(self, result):
         self.result = result
--- Versao3/main.py
+++ Versao3/main.py
@@ -5,8 +5,11 @@ from jogador import jogador
 
 def main():
 
+    # Lista total de 10 jogadores, nomeados 'Jogador1' , "Jogador2", ... , até 'Jogador10'
+    # Todos os jogadores iniciam-se com 100 moedas no saldo
     jogadores = [jogador("Jogador" + str(i), 100, 0) for i in range(1, 11)]
 
+    # Ciclo de rodadas
     while True:
 
         continuar = input("\nNova rodada? (s/n) ")
@@ -14,13 +17,23 @@ def main():
         if continuar != "s":
             break
 
+        # A cada nova rodada, um número de jogadores específico
         numjogadores = int(input("Quantos jogadores nesta rodada? "))
 
+        # Entretanto, o número mínimo de jogadores de cada rodada é 2,
         if numjogadores < 2:
             print("Jogadores insuficientes!")
         
+        # Porém, o máximo de jogadores é 10
+        else if numjogadores > 10:
+            print("Número máximo de jogadores excedido")
+        
+        # Caso as condições acima tenham sido satisfeitas, cria-se uma nova rodada,
+        # com o número de jogadores especificado e a lista dos 10 jogadores
         else:
             mesa = rodada(numjogadores, jogadores)
+            
+            # E tem-se início uma rodada
             mesa.rodada()
 
 
